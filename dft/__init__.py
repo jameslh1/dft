18943bc47e4b21936c958e6ef52b573a3a4f07fb
jameslh1 <j.lloyd-hughes@warwick.ac.uk>
2024-09-10 16:36:41 +0100

    renamed

--- dft/__init__.py
+++ dft/__init__.py
@@ -7,6 +7,6 @@ Python code to perform the discrete Fourier transform, in a way consistent with 
 
 __version__ = "0.0.1"
 
-from .maths import *
+from .base import *
 from .eos import *
 from .plot import *
